7650c687724a3aedaafda7870783f020d34a789e
Charley Wright <charleywright06@gmail.com>
2020-07-09 16:19:45 +0100

    Fixed some relative paths

--- install.sh
+++ install.sh
@@ -15,14 +15,14 @@ echo "| \$\$\$\$\$\$\$/ /\$\$\$\$\$\$|  \$\$\$\$\$\$/   | \$\$   |  \$\$\$\$\$\$
 echo "|_______/ |______/ \______/    |__/    \______/ |__/  |__/|_______/ |________/|_______/\n"
 
 if [ ! -d "/home/$USER/Disturbed" ];then
-    sudo mkdir ~/Disturbed
+    sudo mkdir /home/$USER/Disturbed
     echo "- Created Disturbed directory"
 fi
 
 if [ -f "/home/$USER/Disturbed/Disturbed" ];then
     sudo rm /home/$USER/Disturbed/disturbed
 fi
-sudo wget -P ~/Disturbed/ -q https://raw.githubusercontent.com/charleywright/Disturbed-Linux/master/disturbed
+sudo wget -P /home/$USER/Disturbed/ -q https://raw.githubusercontent.com/charleywright/Disturbed-Linux/master/disturbed
 sudo chown charley /home/$USER/Disturbed/disturbed
 sudo chmod +x /home/$USER/Disturbed/disturbed
 echo "- Downloaded and marked as executable"
